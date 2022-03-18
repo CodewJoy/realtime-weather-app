beb0abea2debffbf2f59dc332cdc1e078d9dfdfc
Joy Hung <yichen.hung6699@gmail.com>
2022-03-18 20:52:17 +0800

    support-dark-theme

--- src/App.js
+++ src/App.js
@@ -3,28 +3,49 @@ import { ReactComponent as DaycloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
-
+import { ThemeProvider } from '@emotion/react';
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 const WeatherCard = styled.div`
+  position: relative;
   min-width: 360px;
-  padding: 15px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
+  box-sizing: border-box;
+  padding: 30px 15px;
 `;
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 const CurrentWeather = styled.div`
@@ -34,7 +55,7 @@ const CurrentWeather = styled.div`
   margin-bottom: 30px;
 `;
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -51,8 +72,9 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
+
   svg {
     width: 25px;
     height: auto;
@@ -64,57 +86,57 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+
   svg {
     width: 25px;
     height: auto;
     margin-right: 30px;
   }
 `;
-const RefreshWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
 const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
-    width: 15px;
-    height: auto;
     margin-left: 10px;
+    width: 15px;
+    height: 15px;
     cursor: pointer;
   }
 `;
 function App() {
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>°C</Celsius>
-          </Temperature>
-          <Daycloudy />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon />
-          23 m/h
-        </AirFlow >
-        <Rain>
-          <RainIcon />
-          48%
-        </Rain>
-        <RefreshWrapper>
-          <Refresh>
-            最後觀測時間
-            <RefreshIcon />
-          </Refresh>
-        </RefreshWrapper >
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme.dark}>
+      <Container>
+        <WeatherCard>
+          <Location>台北市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>
+              23 <Celsius>°C</Celsius>
+            </Temperature>
+            <Daycloudy />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon />
+            23 m/h
+          </AirFlow >
+          <Rain>
+            <RainIcon />
+            48%
+          </Rain>
+            <Refresh>
+              最後觀測時間
+              <RefreshIcon />
+            </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 }
 
